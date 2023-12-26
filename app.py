bb8583a79de39e3799d063801aed4d41cbcdc3e3
shubh9069 <154870800+shubh9069@users.noreply.github.com>
2023-12-26 13:11:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # app.py
 
+import argparse
 from helper import GCP_helper
 
 def upload_file_cloudStorage(filename, bucket_name):
@@ -15,10 +16,18 @@ def download_file_cloudStorage(filename, bucket_name):
     destination_file_name = f"downloaded_{filename}"  # You can modify this based on your requirements
     gcp_helper.download_FILE_CLOUDSTORAGE(source_blob_name, destination_file_name)
 
-if __name__ == "__main__":
-    # Replace these with the actual values
-    argfilename = "req.txt"
-    argbucketName = "bkt-26dec-shubham-v2"
+def main():
+    parser = argparse.ArgumentParser(description="Upload and download files from Google Cloud Storage")
+    parser.add_argument("filename", help="Name of the file to upload/download")
+    parser.add_argument("bucket_name", help="Name of the Google Cloud Storage bucket")
+    parser.add_argument("action", choices=["upload", "download"], help="Action to perform: upload or download")
 
-    upload_file_cloudStorage(argfilename, argbucketName)
-    download_file_cloudStorage(argfilename, argbucketName)
+    args = parser.parse_args()
+
+    if args.action == "upload":
+        upload_file_cloudStorage(args.filename, args.bucket_name)
+    elif args.action == "download":
+        download_file_cloudStorage(args.filename, args.bucket_name)
+
+if __name__ == "__main__":
+    main()

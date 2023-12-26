48cdc2af76ce7c450db90d4b79fb797ca2a97e77
shubh9069 <154870800+shubh9069@users.noreply.github.com>
2023-12-26 13:22:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 # app.py
 
 import argparse
-from helper import GCP_helper
+# from helper import GCP_helper
 
 def upload_file_cloudStorage(filename, bucket_name):
-    project_id = "your-project-id"
+    project_id = "manipalpr-1677473930500"
     gcp_helper = GCP_helper(project_id, bucket_name)
     destination_blob_name = filename  # You can modify this based on your requirements
     gcp_helper.upload_File_Cloud_Storage(filename, destination_blob_name)
 
 def download_file_cloudStorage(filename, bucket_name):
-    project_id = "your-project-id"
+    project_id = "manipalpr-1677473930500"
     gcp_helper = GCP_helper(project_id, bucket_name)
     source_blob_name = filename  # You can modify this based on your requirements
     destination_file_name = f"downloaded_{filename}"  # You can modify this based on your requirements
